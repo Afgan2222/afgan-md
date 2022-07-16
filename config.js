887fc7ae73a8abf5c60f1c0d7261013a943e7806
Afgan2222 <108980042+Afgan2222@users.noreply.github.com>
2022-07-16 21:41:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,17 +31,17 @@ global.APIKeys = { // APIKey Here
   'https://api.adiofficial.xyz': 'apikey lu'
 }
 
-//global.wm = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆'
+//global.wm = 'kanna BOT - MD'
 global.wait = '_*𝒔𝒆𝒅𝒂𝒏𝒈 𝒅𝒊 𝒑𝒓𝒐𝒔𝒆𝒔...*_'
 global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓...*_'
 
 //========Url Template Buttons=========//
-global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.dtu = 'source code'
+global.urlnya = "https://chat.whatsapp.com/DVfawsZozt36QdYmfQlsbD"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '62 857-1313-7903'
+global.phn = '62 821-2596-8810'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
